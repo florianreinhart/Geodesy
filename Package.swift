7317967b498b4bf408d162e823056e7ab2f1d2b5
kevinschlieper <kevin@bottledsoftware.de>
2017-12-06 15:51:19 +0100

    Change library name from Geodesy to GeodesySpherical

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Geodesy",
     products: [
         .library(
-            name: "Geodesy",
+            name: "GeodesySpherical",
             targets: ["GeodesySpherical"]),
     ],
     dependencies: [

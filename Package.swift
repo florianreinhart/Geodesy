0352c1661966e45ecd912e3d99262c055277ad71
kevinschlieper <kevin@bottledsoftware.de>
2017-12-06 16:00:26 +0100

    Update Package.swift file (#1)

--- Package.swift
+++ Package.swift
@@ -1,5 +1,22 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "Geodesy"
+    name: "Geodesy",
+    products: [
+        .library(
+            name: "Geodesy",
+            targets: ["GeodesySpherical"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "GeodesySpherical",
+            dependencies: []),
+        .testTarget(
+            name: "GeodesySphericalTests",
+            dependencies: ["GeodesySpherical"]),
+    ]
 )
